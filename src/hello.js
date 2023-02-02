fb1d96f351f315bff18a25af9b57c56c42064693
azizsnoussi <63208771+azizsnoussi@users.noreply.github.com>
2023-02-02 09:27:27 +0100

    first push

---
+++ src/hello.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function hello() {
+  return (
+    <>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </>
+  )
+}
+
+
+
